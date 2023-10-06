2270adbab6257f8583e3196ff1584b84ca5cb339
Jonatas Pinheiro Mendes <jonatas.mendes@fitbank.com.br>
2023-10-06 10:47:06 -0300

    Ajustes

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
+ary = [];
+
+function Televisao(modelo, marca, tipo, Qntd, condicao, diferencial){
+    this.Modelo = modelo;
+    this.Marca = marca;
+    this.Tipo =  tipo;
+    this.Quantidade =  Qntd;
+    this.Codicao =  condicao;
+    this.diferencial =  diferencial;
+}
 
 
 function GetRegisterItensScreen() {
-    document.getElementById("NewItemForm").removeAttribute("hidden");
+    document.getElementById("NewItemForm").hidden = false;
+    document.getElementById("ListItens").hidden = true;
 }
 
 function GetListItensScreen() {
+    document.getElementById("NewItemForm").hidden = true;
+    document.getElementById("ListItens").hidden = false;
 
-    var attr = document.createAttribute('hidden');
-    document.getElementById("NewItemForm").setAttributeNode(attr);
-    debugger;
-    var jesus
+    document.getElementById("showList").innerHTML = '<tr><th>Modelo</th><th>Marca</th><th>Qntd.</th><th>Actions</th></tr>'
 
     for(ar in ary)
-        jesus += `<tr> <td>${ar.Modelo}<td> </td>${ar.Marca}</td> <td>${ar.Quantidade}</td> </tr>`;
-
-    document.getElementById("Listitem").append(jesus)
+        document.getElementById("showList").innerHTML += `<tr> <td>${ary[ar].Modelo}</td> <td>${ary[ar].Marca}</td> <td>${ary[ar].Quantidade}</td> <td><button>Edit</button><button>Test</button><button>Remove</button></td> </tr>`;
 }
-
-
-
-ary = [];
 
 function AddItem(){
     var form = document.getElementById("myForm");
 
-    itenObj = {
-        'Modelo': form.model.value,
-        'Marca': form.brand.value,
-        'Tipo': form.typeselected.value,
-        'Quantidade': form.quantity.value,
-        'Codicao': form.condition.value,
-        'diferencial': form.diff.valie
-    };
-
-    ary.push(itenObj)
+    ary.push(new Televisao(form.model.value, form.brand.value, form.typeselected.value, form.quantity.value, form.condition.value, form.diff.valie))
     alert("Adicionado")
-
-    return false;
 }
